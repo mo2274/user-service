84937e935540f6eaf1d0a874f7b7738160a30acb
mo2274 <mohamedzaghlol74111@gmail.com>
2021-09-15 22:07:59 +0200

    add files

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username':"postgres",
+  'password': "mohamed22",
+  'database': "postgres",
+  'host': "database-1.cnefbvgpg12h.us-east-1.rds.amazonaws.com",
+  'dialect': 'postgres',
+  'aws_region': "us-east-1",
+  'aws_profile': "474796527302",
+  'aws_media_bucket': "udgrambucketmo2274prod",
+  'url': "http://localhost:8081",
+  'jwt': {
+    'secret': "hello",
+  },
+};
